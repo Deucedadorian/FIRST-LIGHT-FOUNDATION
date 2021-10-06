05d1427e5df58187422ee3a14a53e3744b594c22
Dylan Cole <logboarddjc@gmail.com>
2021-10-05 20:59:21 -0400

    add about component

---
+++ src/components/About.js
@@ -0,0 +1,215 @@
+const About = () => {
+  return (
+    <main class="centered-bottom" id="main" role="main" tabindex="-1">
+      <div class="main section" id="page_body" name="Page Body">
+        <div class="widget Blog" data-version="2" id="Blog1">
+          <div class="blog-posts hfeed container">
+            <div class="post-outer-container">
+              <div class="post-outer">
+                <div class="post-sidebar">
+                  <div class="post-sidebar-item post-share-buttons">
+                    <div aria-owns="sharing-popup-Blog1-byline-5823337226810332231" class="sharing" data-title="">
+                      <button aria-controls="sharing-popup-Blog1-byline-5823337226810332231" aria-label="Share" class="sharing-button touch-icon-button" id="sharing-button-Blog1-byline-5823337226810332231" aria-expanded="false" aria-haspopup="true">
+                        Share
+                      </button>
+                      <div class="share-buttons-container">
+                        <ul aria-hidden="true" aria-label="Share" class="share-buttons hidden" id="sharing-popup-Blog1-byline-5823337226810332231" role="menu">
+                          <li>
+                            <span aria-label="Get link" class="sharing-platform-button sharing-element-link" data-href="https://www.blogger.com/share-post.g?blogID=5795806888231951572&amp;pageID=5823337226810332231&amp;target=" data-url="http://www.firstlightfoundation.net/p/annawon-weeden-bio-page.html" role="menuitem" tabindex="-1" title="Get link">
+                              <svg class="svg-icon-24 touch-icon sharing-link">
+                                <use xlink:href="/responsive/sprite_v1_6.css.svg#ic_24_link_dark" xmlns:xlink="http://www.w3.org/1999/xlink"></use>
+                              </svg>
+                              <span class="platform-sharing-text">Get link</span>
+                            </span>
+                          </li>
+                          <li>
+                            <span aria-label="Share to Facebook" class="sharing-platform-button sharing-element-facebook" data-href="https://www.blogger.com/share-post.g?blogID=5795806888231951572&amp;pageID=5823337226810332231&amp;target=facebook" data-url="http://www.firstlightfoundation.net/p/annawon-weeden-bio-page.html" role="menuitem" tabindex="-1" title="Share to Facebook">
+                              <svg class="svg-icon-24 touch-icon sharing-facebook">
+                                <use xlink:href="/responsive/sprite_v1_6.css.svg#ic_24_facebook_dark" xmlns:xlink="http://www.w3.org/1999/xlink"></use>
+                              </svg>
+                              <span class="platform-sharing-text">Facebook</span>
+                            </span>
+                          </li>
+                          <li>
+                            <span aria-label="Share to Twitter" class="sharing-platform-button sharing-element-twitter" data-href="https://www.blogger.com/share-post.g?blogID=5795806888231951572&amp;pageID=5823337226810332231&amp;target=twitter" data-url="http://www.firstlightfoundation.net/p/annawon-weeden-bio-page.html" role="menuitem" tabindex="-1" title="Share to Twitter">
+                              <svg class="svg-icon-24 touch-icon sharing-twitter">
+                                <use xlink:href="/responsive/sprite_v1_6.css.svg#ic_24_twitter_dark" xmlns:xlink="http://www.w3.org/1999/xlink"></use>
+                              </svg>
+                              <span class="platform-sharing-text">Twitter</span>
+                            </span>
+                          </li>
+                          <li>
+                            <span aria-label="Share to Pinterest" class="sharing-platform-button sharing-element-pinterest" data-href="https://www.blogger.com/share-post.g?blogID=5795806888231951572&amp;pageID=5823337226810332231&amp;target=pinterest" data-url="http://www.firstlightfoundation.net/p/annawon-weeden-bio-page.html" role="menuitem" tabindex="-1" title="Share to Pinterest">
+                              <svg class="svg-icon-24 touch-icon sharing-pinterest">
+                                <use xlink:href="/responsive/sprite_v1_6.css.svg#ic_24_pinterest_dark" xmlns:xlink="http://www.w3.org/1999/xlink"></use>
+                              </svg>
+                              <span class="platform-sharing-text">Pinterest</span>
+                            </span>
+                          </li>
+                          <li>
+                            <span aria-label="Email" class="sharing-platform-button sharing-element-email" data-href="https://www.blogger.com/share-post.g?blogID=5795806888231951572&amp;pageID=5823337226810332231&amp;target=email" data-url="http://www.firstlightfoundation.net/p/annawon-weeden-bio-page.html" role="menuitem" tabindex="-1" title="Email">
+                              <svg class="svg-icon-24 touch-icon sharing-email">
+                                <use xlink:href="/responsive/sprite_v1_6.css.svg#ic_24_email_dark" xmlns:xlink="http://www.w3.org/1999/xlink"></use>
+                              </svg>
+                              <span class="platform-sharing-text">Email</span>
+                            </span>
+                          </li>
+                          <li aria-hidden="true" class="hidden">
+                            <span aria-label="Share to other apps" class="sharing-platform-button sharing-element-other" data-url="http://www.firstlightfoundation.net/p/annawon-weeden-bio-page.html" role="menuitem" tabindex="-1" title="Share to other apps">
+                              <svg class="svg-icon-24 touch-icon sharing-sharingOther">
+                                <use xlink:href="/responsive/sprite_v1_6.css.svg#ic_more_horiz_black_24dp" xmlns:xlink="http://www.w3.org/1999/xlink"></use>
+                              </svg>
+                              <span class="platform-sharing-text">Other Apps</span>
+                            </span>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="post">
+                  {/* <script type="application/ld+json">{
+                    "@context": "http://schema.org",
+                    "@type": "BlogPosting",
+                    "mainEntityOfPage": {
+                      "@type": "WebPage",
+                    "@id": "http://www.firstlightfoundation.net/p/annawon-weeden-bio-page.html"
+  },
+                    "headline": "Annawon Weeden","description": "Annawon Weeden is an enrolled member of his mother\u0026#39;s Mashpee Wampanoag tribal community located on Cape Cod. He current...","datePublished": "2017-11-28T00:12:00-05:00",
+                    "dateModified": "2017-11-28T00:12:36-05:00","image": {
+                      "@type": "ImageObject","url": "https://2.bp.blogspot.com/-Ci7jmD26ez0/WhtdF3o2sgI/AAAAAAABLzA/aTwCehhv-CA5G6UZkNHgPg_O76czxhEcgCLcBGAs/w1200-h630-p-k-no-nu/image3.jpeg",
+                    "height": 630,
+    "width": 1200},"publisher": {
+                      "@type": "Organization",
+                    "name": "Blogger",
+                    "logo": {
+                      "@type": "ImageObject",
+                    "url": "https://lh3.googleusercontent.com/ULB6iBuCeTVvSjjjU1A-O8e9ZpVba6uvyhtiWRti_rBAs9yMYOFBujxriJRZ-A=h60",
+                    "width": 206,
+                    "height": 60
+    }
+  },"author": {
+                      "@type": "Person",
+                    "name": "Ogitchida Kwe's Book Blog"
+  }
+}</script> */}
+                  <h3 class="post-title entry-title">
+                    Annawon Weeden
+                  </h3>
+                  <div class="post-body-container">
+                    <div class="post-body entry-content float-container" id="post-body-5823337226810332231">
+                      <br></br>
+                      {/* style="text-align: center;" */}
+                      <div >
+                      <br></br>
+                    </div>
+                      <br></br>
+                      {/* style="clear: both; text-align: center;" */}
+                    <div class ="separator" ></div>
+                      <br></br>
+                      {/* style="clear: both; text-align: center;" */}
+                    <div class ="separator" >
+                    {/* style="margin-left: 1em; margin-right: 1em;" this was from the a tag below */}
+                      <a href="https://2.bp.blogspot.com/-Ci7jmD26ez0/WhtdF3o2sgI/AAAAAAABLzA/aTwCehhv-CA5G6UZkNHgPg_O76czxhEcgCLcBGAs/s1600/image3.jpeg" imageanchor="1"><img border="0" data-original-height="1600" data-original-width="1554" height="640" src="https://2.bp.blogspot.com/-Ci7jmD26ez0/WhtdF3o2sgI/AAAAAAABLzA/aTwCehhv-CA5G6UZkNHgPg_O76czxhEcgCLcBGAs/s640/image3.jpeg" width="621" alt="this is Annawon dressed in authentic wompanoag clothes" /></a></div>
+                      {/* style="text-align: center;" */}
+                      <div >
+                        <br></br>
+                      </div>
+                      <br></br>
+                      {/* style="background-color: white; color: #222222; font-family: arial, sans-serif;" */}
+                      <blockquote  type ="cite">
+                      <div class ="gmail_extra">
+                      <div class ="gmail_quote">
+                      {/* style="border-left: 1px solid rgb(204, 204, 204); margin: 0px 0px 0px 0.8ex; padding-left: 1ex;" */}
+                      <blockquote class ="gmail_quote" >
+                      <div dir="auto">
+                      {/* style="background-color: rgba(255, 255, 255, 0);" style="font-size: large;" */}
+                      <span style><span style>Annawon Weeden is an enrolled member of his mother's Mashpee Wampanoag tribal community located on Cape Cod. He currently works in the MPTN Cultural Resource Dept. as the Eastern Woodland song/dance instructor for his father's Mashantucket Pequot Tribal community located on their reservation in southeastern CT.Growing up on the Narragansett reservation in south coastal RI, Annawon was instructed on the traditional dances &amp; customs of New England natives throughout his entire life.As an adult, traveling abroad visiting many other tribes across the U.S., Annawon has developed a comprehensive understanding of the vast diversity among many native customs.Annawon looks forward to any opportunity to share his cultural knowledge with public audiences while engaging others in cultural preservation for future generations of better understanding.&nbsp; </span></span></div>
+                      </blockquote>
+                      </div>
+                      </div>
+                      </blockquote>
+                      </div>
+                      </div>
+                      <div class ="post-footer">
+                      <div class ="post-footer-line post-footer-line-1">
+                      <div class ="byline post-share-buttons goog-inline-block">
+                      <div aria-owns="sharing-popup-Blog1-footer-1-5823337226810332231" class ="sharing" data-title="Annawon Weeden">
+                      <button aria-controls="sharing-popup-Blog1-footer-1-5823337226810332231" aria-label="Share" class ="sharing-button touch-icon-button" id="sharing-button-Blog1-footer-1-5823337226810332231" aria-expanded="false" aria-haspopup="true">
+                      Share
+                      </button>
+                      <div class ="share-buttons-container">
+                      <ul aria-hidden="true" aria-label="Share" class ="share-buttons hidden" id="sharing-popup-Blog1-footer-1-5823337226810332231" role="menu">
+                      <li>
+                      <span aria-label="Get link" class ="sharing-platform-button sharing-element-link" data-href="https://www.blogger.com/share-post.g?blogID=5795806888231951572&amp;pageID=5823337226810332231&amp;target=" data-url="http://www.firstlightfoundation.net/p/annawon-weeden-bio-page.html" role="menuitem" tabindex="-1" title="Get link">
+                      <svg class ="svg-icon-24 touch-icon sharing-link">
+                      {/* <use xlink: href="/responsive/sprite_v1_6.css.svg#ic_24_link_dark" xmlns: xlink="http://www.w3.org/1999/xlink"></use> */}
+                      <use href="/responsive/sprite_v1_6.css.svg#ic_24_link_dark" link="http://www.w3.org/1999/xlink"></use>
+                      </svg>
+                      <span class ="platform-sharing-text">Get link</span>
+                      </span>
+                      </li>
+                      <li>
+                      <span aria-label="Share to Facebook" class ="sharing-platform-button sharing-element-facebook" data-href="https://www.blogger.com/share-post.g?blogID=5795806888231951572&amp;pageID=5823337226810332231&amp;target=facebook" data-url="http://www.firstlightfoundation.net/p/annawon-weeden-bio-page.html" role="menuitem" tabindex="-1" title="Share to Facebook">
+                      <svg class ="svg-icon-24 touch-icon sharing-facebook">
+                      {/* <use xlink: href="/responsive/sprite_v1_6.css.svg#ic_24_facebook_dark" xmlns: xlink="http://www.w3.org/1999/xlink"></use> */}
+                      <use href="/responsive/sprite_v1_6.css.svg#ic_24_facebook_dark" link="http://www.w3.org/1999/xlink"></use>
+                      </svg>
+                      <span class ="platform-sharing-text">Facebook</span>
+                      </span>
+                      </li>
+                      <li>
+                      <span aria-label="Share to Twitter" class ="sharing-platform-button sharing-element-twitter" data-href="https://www.blogger.com/share-post.g?blogID=5795806888231951572&amp;pageID=5823337226810332231&amp;target=twitter" data-url="http://www.firstlightfoundation.net/p/annawon-weeden-bio-page.html" role="menuitem" tabindex="-1" title="Share to Twitter">
+                      <svg class ="svg-icon-24 touch-icon sharing-twitter">
+                      {/* <use xlink: href="/responsive/sprite_v1_6.css.svg#ic_24_twitter_dark" xmlns: xlink="http://www.w3.org/1999/xlink"></use> */}
+                      <use href="/responsive/sprite_v1_6.css.svg#ic_24_twitter_dark" xlink="http://www.w3.org/1999/xlink"></use>
+                      </svg>
+                      <span class ="platform-sharing-text">Twitter</span>
+                      </span>
+                      </li>
+                      <li>
+                      <span aria-label="Share to Pinterest" class ="sharing-platform-button sharing-element-pinterest" data-href="https://www.blogger.com/share-post.g?blogID=5795806888231951572&amp;pageID=5823337226810332231&amp;target=pinterest" data-url="http://www.firstlightfoundation.net/p/annawon-weeden-bio-page.html" role="menuitem" tabindex="-1" title="Share to Pinterest">
+                      <svg class ="svg-icon-24 touch-icon sharing-pinterest">
+                      <use href="/responsive/sprite_v1_6.css.svg#ic_24_pinterest_dark" link="http://www.w3.org/1999/xlink"></use>
+                      </svg>
+                      <span class ="platform-sharing-text">Pinterest</span>
+                      </span>
+                      </li>
+                      <li>
+                      <span aria-label="Email" class ="sharing-platform-button sharing-element-email" data-href="https://www.blogger.com/share-post.g?blogID=5795806888231951572&amp;pageID=5823337226810332231&amp;target=email" data-url="http://www.firstlightfoundation.net/p/annawon-weeden-bio-page.html" role="menuitem" tabindex="-1" title="Email">
+                      <svg class ="svg-icon-24 touch-icon sharing-email">
+                      <use href="/responsive/sprite_v1_6.css.svg#ic_24_email_dark" xlink="http://www.w3.org/1999/xlink"></use>
+                      </svg>
+                      <span class ="platform-sharing-text">Email</span>
+                      </span>
+                      </li>
+                      <li aria-hidden="true" class ="hidden">
+                      <span aria-label="Share to other apps" class ="sharing-platform-button sharing-element-other" data-url="http://www.firstlightfoundation.net/p/annawon-weeden-bio-page.html" role="menuitem" tabindex="-1" title="Share to other apps">
+                      <svg class ="svg-icon-24 touch-icon sharing-sharingOther">
+                      <use href="/responsive/sprite_v1_6.css.svg#ic_more_horiz_black_24dp" xlink="http://www.w3.org/1999/xlink"></use>
+                      </svg>
+                      <span class ="platform-sharing-text">Other Apps</span>
+                      </span>
+                      </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="post-footer-line post-footer-line-2">
+              </div>
+            </div>
+          </div>
+        </div>
+        <section class="comments" data-num-comments="0" id="comments">
+          {/* <a name="comments"></a> */}
+        </section>
+      </div>
+    </div>
+                        </div >
+                    </div >
+                </main >
+                )
+}
+
+export default About;
