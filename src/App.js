b4cf2705d2abb5f9252b72169d9e3fe8e3af1229
Dylan Cole <logboarddjc@gmail.com>
2021-10-07 15:02:34 -0400

    edit FunHeader

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import FoundationHeader from './components/FoundationHeader';
+import FunHeader from './components/FunHeader';
 import Aside from './components/Aside';
-import Home from './components/Home'
+import Home from './components/Home';
+import BookEvent from './components/BookEvent';
 
 function App() {
   return (
     <div className="App">
       <FoundationHeader />
+      <FunHeader />
       <Home />
       <Aside />
+      <BookEvent />
     </div>
   );
 }
