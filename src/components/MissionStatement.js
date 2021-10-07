0cfbb94eb3bfa7cd445f94be8acead328e553681
Dylan Cole <logboarddjc@gmail.com>
2021-10-07 18:23:39 -0400

    slight change on missionstatement.js

--- src/components/MissionStatement.js
+++ src/components/MissionStatement.js
@@ -5,19 +5,17 @@ const MissionStatement = () => {
           <h3 className="post-title entry-title">
             Mission Statement
           </h3>
-          <div className="post-body-container">
-            <div className="post-body entry-content float-container">
-              <div>
-                <p>The First Light Foundation is here to reinforce the identity of each individual served. 
-                  All across the globe, society is constantly evolving. Keeping up with modern trends can easily 
-                  take the place of longstanding traditional values. Our goal at First Light Foundation is to highlight 
-                  the importance of celebrating diversity.&nbsp; Promoting healthy minds, bodies &amp; spirits through 
-                  environmental resources, educational programming &amp; cultural arts, we approach identity from all angles. 
-                  Seven generation preservation, all across Turtle Island &amp; beyond. We strive to spark new light on the 
-                  road to a brighter future, for all our relations. We thank you for supporting our cause and invite you to 
-                  take part in building a strong foundation for our future to stand on.
-                </p>
-              </div>
+          <div className="post-body entry-content float-container">
+            <div>
+              <p>The First Light Foundation is here to reinforce the identity of each individual served. 
+                All across the globe, society is constantly evolving. Keeping up with modern trends can easily 
+                take the place of longstanding traditional values. Our goal at First Light Foundation is to highlight 
+                the importance of celebrating diversity.&nbsp; Promoting healthy minds, bodies &amp; spirits through 
+                environmental resources, educational programming &amp; cultural arts, we approach identity from all angles. 
+                Seven generation preservation, all across Turtle Island &amp; beyond. We strive to spark new light on the 
+                road to a brighter future, for all our relations. We thank you for supporting our cause and invite you to 
+                take part in building a strong foundation for our future to stand on.
+              </p>
             </div>
           </div>
         </div>
