52ed7154a72248e864447206bdc282d4e286bdc5
Dylan Cole <logboarddjc@gmail.com>
2021-10-07 15:21:06 -0400

    refactor Aside

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -3,12 +3,6 @@ const Aside = () => {
     <aside className="sidebar-container container sidebar-invisible">
       <div className="navigation">
         <svg className="svg-icon-24 touch-icon sidebar-back rtl-reversible-icon">
-          {/*
-          // this use tag is the OG. refactored to work with react.
-          <use
-            xlink:href="/responsive/sprite_v1_6.css.svg#ic_arrow_back_black_24dp"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-          ></use> */}
           <use
             href="/responsive/sprite_v1_6.css.svg#ic_arrow_back_black_24dp"
             link="http://www.w3.org/1999/xlink"
