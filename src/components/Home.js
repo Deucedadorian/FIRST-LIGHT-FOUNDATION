b14749fe647f5a65ad5fcc738c85d6f3c6dd9d07
Dylan Cole <logboarddjc@gmail.com>
2021-10-07 18:22:30 -0400

    refactor Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,50 +2,20 @@ const Home = () => {
     return (
       <main className="centered-bottom" id="main" role="main">
         <div className="main section" id="page_body" name="Page Body">
-          <div className="widget Blog" data-version="2" id="Blog1">
-            <div className="blog-posts hfeed container">
-              <div className="post-outer-container" >
-                <div className="post-outer">
-                  <div className="post no-featured-image">
-                    <div className="post-header">
-                      <div className="post-header-line-1">
-                      </div>
-                    </div>
-                    <h3 className="post-title entry-title">
-                      <a href="http://www.firstlightfoundation.net/2019/09/gearing-up-for-2019-2020-school-season.html">Gearing up for 2019-2020 school season! </a>
-                    </h3>
-                  </div>
-                </div>
-              </div>
-              <div className="post-outer-container">
-                <div className="post-outer">
-                  <div className="post no-featured-image">
-                    <div className="post-header">
-                      <div className="post-header-line-1">
-                      </div>
-                    </div>
-                    <h3 className="post-title entry-title">
-                      <a href="http://www.firstlightfoundation.net/2019/02/colonial-day-revolution.html">Colonial Day Revolution</a>
-                    </h3>
-                  </div>
-                </div>
-              </div>
-              <div className="post-outer-container">
-                <div className="post-outer">
-                  <div className="post no-featured-image">
-                    <div className="post-header">
-                      <div className="post-header-line-1">
-                      </div>
-                    </div>
-                    <h3 className="post-title entry-title">
-                      <a href="http://www.firstlightfoundation.net/2017/11/judgement-day.html">Judgement Day </a>
-                    </h3>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="blog-pager container" id="blog-pager">
-            </div>
+          <div className="article-card">
+            <h3 className="post-title entry-title">
+              <a href="http://www.firstlightfoundation.net/2019/09/gearing-up-for-2019-2020-school-season.html">Gearing up for 2019-2020 school season!</a>
+            </h3>
+          </div>
+          <div className="article-card">
+            <h3 className="post-title entry-title">
+              <a href="http://www.firstlightfoundation.net/2019/02/colonial-day-revolution.html">Colonial Day Revolution</a>
+            </h3>
+          </div>
+          <div className="article-card">
+            <h3 className="post-title entry-title">
+              <a href="http://www.firstlightfoundation.net/2017/11/judgement-day.html">Judgement Day</a>
+            </h3>
           </div>
         </div>
       </main>
