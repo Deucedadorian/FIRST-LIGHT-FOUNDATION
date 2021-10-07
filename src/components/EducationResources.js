50d40aadacc91444f9734b8c89306965f5864b8d
Dylan Cole <logboarddjc@gmail.com>
2021-10-07 18:44:21 -0400

    add educationresources.js

---
+++ src/components/EducationResources.js
@@ -0,0 +1,71 @@
+const educationResources = () => {
+  return (
+    <div className="post">
+      <div className="post-header">
+        <div className="post-header-line-1">
+        </div>
+      </div>
+      <h3 className="post-title entry-title">
+        Educational Resources
+      </h3>
+      <div className="post-body-container">
+        <div className="post-body entry-content float-container">
+          <div>
+            For Educators:</div>
+          <div>
+            1621: A New Look At Thanksgiving/ National Geographic (2004)</div>
+          <div>
+            Spirit of the New England Tribes/William Simmons &nbsp;(1986)</div>
+          <div>
+            Key Into the Language of America/Roger Williams (1997)</div>
+          <div>
+            Mourt’s Relation / Edward Winslow </div>
+          <div>
+            Of Plimoth Plantation / William Bradford</div>
+          <div>
+            Three Visitors to New Plymouth/&nbsp;&nbsp;Emmanuel Altham</div>
+          <div></div>
+          <div>
+            In the Name of War / Jill Lepore&nbsp;(1999)</div>
+          <div>
+            <div>Children’s Books:</div>
+            <div>
+              “Tapenum’s Day” by Kate Waters (1996)</div>
+            <div>
+              “Clambake” by Russell Peters (1992)</div>
+            <div>
+              Giving Thanks by Chief Jake Swamp (1997)</div>
+            <div>
+              Cranberry Day by&nbsp;Jannette Vanderhoop (2002)</div>
+            <div>
+              Strawberry Thanksgiving: Multicultural Celebrations by Paula Dove Jennings (1992) 
+            </div>
+          </div>
+          <div>Cultural Content</div>
+          <div>
+            <div>
+              <a href="https://en.m.wikipedia.org/wiki/Ancient_Fishweir_Project">https://en.m.wikipedia.org/wiki/Ancient_Fishweir_Project</a></div>
+          </div>
+          <div>
+            <div>
+              <a href="https://web.archive.org/web/20120930103508/http:/www.capecodonline.com/apps/pbcs.dll/article?AID=/20020616/NEWS01/306169954&amp;cid=sitesearch">https://web.archive.org/web/20120930103508/http://www.capecodonline.com/apps/pbcs.dll/article?AID=/20020616/NEWS01/306169954&amp;cid=sitesearch</a>&nbsp;</div>
+          </div>
+          <div>
+            <div>
+              <a href="http://www.capecodtimes.com/article/20020714/NEWS01/307149989">http://www.capecodtimes.com/article/20020714/NEWS01/307149989</a></div>
+          </div>
+          <div>
+            <div>
+              <a href="http://www.capecodtimes.com/article/20020818/news01/308189979">http://www.capecodtimes.com/article/20020818/news01/308189979</a></div>
+          </div>
+          <div>
+            <div>
+              <a href="http://www.echospace.org/">http://www.echospace.org/</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default educationResources;
