e58e013feb4b55779404dc0c2be3cba1fc95ce75
Dylan Cole <logboarddjc@gmail.com>
2021-10-07 19:05:55 -0400

    export reviews.js

---
+++ src/components/Reviews.js
@@ -0,0 +1,16 @@
+const Reviews = () => {
+  return (
+    <div class="post">
+      <h3 class="post-title entry-title">
+        Reviews
+      </h3>
+      <div class="post-body-container">
+        <div class="post-body entry-content float-container" id="post-body-2989664963729614824">
+          <a href="https://4.bp.blogspot.com/-TfvVZrBMG2M/WhtVBiWsjXI/AAAAAAABLyg/8SpoOKplcqMfkSJeOShtQz73irHgUrteQCPcBGAYYCw/s1600/First%2BLight%2B%25285%2529.png" imageanchor="1"><img border="0" data-original-height="789" data-original-width="940" height="269" src="https://4.bp.blogspot.com/-TfvVZrBMG2M/WhtVBiWsjXI/AAAAAAABLyg/8SpoOKplcqMfkSJeOShtQz73irHgUrteQCPcBGAYYCw/s320/First%2BLight%2B%25285%2529.png" width="320"  alt="Annawon's logo"/></a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Reviews;
