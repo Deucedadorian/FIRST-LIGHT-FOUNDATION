a26a70163b4d5ba76634f0662f533a8137339e87
Dylan Cole <logboarddjc@gmail.com>
2021-10-07 15:19:21 -0400

    refactor foundationHeader

--- src/components/FoundationHeader.js
+++ src/components/FoundationHeader.js
@@ -4,52 +4,25 @@ const FoundationHeader = () => {
       <div className="centered-top">
         <div className="hamburger-menu-container">
           <svg className="svg-icon-24 touch-icon hamburger-menu">
-            {/* The Following line is the full line copied from the original site. 
-            xlink:... and xmlns:... didn't work with react so the next line takes it out. 
-            Just wanted to keep track of it incase I needed to add it back in some how.*/}
-            {/* <use xlink:href="/responsive/sprite_v1_6.css.svg#ic_menu_black_24dp" xmlns:xlink="http://www.w3.org/1999/xlink"></use> */}
             <use href="/responsive/sprite_v1_6.css.svg#ic_menu_black_24dp" link="http://www.w3.org/1999/xlink"></use>
           </svg>
         </div>
         <div className="blog-name">
-          <div className="section" id="header" name="Header"><div className="widget Header" data-version="2" id="Header1">
-            <div className="header-widget">
-              <div>
-                <h1>
-                  First Light Foundation
-                </h1>
-              </div>
-              <p>
-                “Our future is only as strong as the foundation it’s built upon”
-
-              </p>
-            </div>
-          </div></div>
-          <nav role="navigation">
-            <div className="section" id="page_list_top" name="Page List (Top)">
-              <div className="widget PageList" data-version="2" id="PageList1">
-                <h3 className="title">
-                  Pages
-                </h3>
-                <div className="widget-content">
-                  <div className="overflowable-container overflowable-2">
-                    <div className="overflowable-contents">
-                      <div className="container">
-                        <ul className="tabs decoration-none">
-                          {/* <li className="overflow-button-container" style={"display: none;"} aria-hidden="true"><div className="overflow-button">
-                            <a>More…</a>
-                          </div></li> */}
-                          <li className="overflowable-item selected" aria-hidden="false">
-                            <a href="/" className="decoration-none">Home</a>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
+          <div className="section" id="header" name="Header">
+            <div className="widget Header" data-version="2" id="Header1">
+              <div className="header-widget">
+                <div>
+                  <h1>
+                    First Light Foundation
+                  </h1>
                 </div>
+                <p>
+                  “Our future is only as strong as the foundation it’s built upon”
+
+                </p>
               </div>
             </div>
-          </nav>
+          </div>
         </div>
       </div>
     </header>
