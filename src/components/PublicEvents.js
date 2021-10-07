a4fdde66b32a003f2318b62742c3b75fbc8bdc2f
Dylan Cole <logboarddjc@gmail.com>
2021-10-07 18:59:34 -0400

    add publicevents.js

---
+++ src/components/PublicEvents.js
@@ -0,0 +1,17 @@
+const PublicEvents = () => {
+  return (
+    <div className="post">
+      <div className="title">
+        <h3 className="post-title entry-title">
+          Public Events
+        </h3>
+      </div>
+      <div className="body-container">
+        <a href="https://2.bp.blogspot.com/-6ayF3m7Xi7U/WuQJB0rVJYI/AAAAAAABQWg/D4Xp6ZS4IBQaZJiBXJ4MhEaYh_-YBBVAwCLcBGAs/s1600/image1.jpeg" imageanchor="1"><img border="0" data-original-height="1055" data-original-width="1600" height="422" src="https://2.bp.blogspot.com/-6ayF3m7Xi7U/WuQJB0rVJYI/AAAAAAABQWg/D4Xp6ZS4IBQaZJiBXJ4MhEaYh_-YBBVAwCLcBGAs/s640/image1.jpeg" width="640" /></a>
+        <h1>Intertribal</h1>
+        <h1>&nbsp; Food Sovereignty Summit</h1>
+        <h1>August 20-25, 2018</h1>  
+      </div>
+    </div>
+  )
+}
