c906559925e4d538c8f9d391c372e9925f6405af
Dylan Cole <logboarddjc@gmail.com>
2021-10-07 15:40:14 -0400

    refactor MissionStatement component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import FoundationHeader from './components/FoundationHeader';
 import FunHeader from './components/FunHeader';
+import About from './components/About';
 import Aside from './components/Aside';
 import Home from './components/Home';
 import BookEvent from './components/BookEvent';
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <FoundationHeader />
+      <About />
       <FunHeader />
       <Home />
       <Aside />
