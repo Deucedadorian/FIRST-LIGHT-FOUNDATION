53112b24c344f7950d9e7266a571d4636ea5031f
Dylan Cole <logboarddjc@gmail.com>
2021-10-06 22:04:44 -0400

    refactored BookEvent.js

---
+++ src/components/BookEvent.js
@@ -0,0 +1,37 @@
+const BookEvent = () => {
+  return (
+    <main class="centered-bottom" id="main" role="main" tabindex="-1">
+      <div class="main section" id="page_body" name="Page Body">
+        <div class="widget Blog" data-version="2" id="Blog1">
+          <div class="blog-posts hfeed container">
+            <h3 class="post-title entry-title">
+              Book Your Event
+            </h3>
+            <div class="post-body-container">
+              <a href="https://4.bp.blogspot.com/-f_RtQGaipUo/WhzQpjn0wVI/AAAAAAABL7Q/MEKf11XC5R0ruYfw23E9EkV9HBYktXr_gCLcBGAs/s1600/Bookings.png" imageanchor="1"><img border="0" data-original-height="756" data-original-width="945" height="512" src="https://4.bp.blogspot.com/-f_RtQGaipUo/WhzQpjn0wVI/AAAAAAABL7Q/MEKf11XC5R0ruYfw23E9EkV9HBYktXr_gCLcBGAs/s640/Bookings.png" width="640" alt="bookings poster"/></a>
+              <span>*WEEKDAY APPOINTMENTS*</span>
+              <div>
+                <a href="https://my.setmore.com/bookanappointmentv3.do?uniqueKey=d17471a9-0a65-4a79-b3ac-3065eff617f8" target="_blank" rel="noreferrer"><button>Setmore</button></a>
+                <span>SPECIAL NOTE FOR WEEKEND APPOINTMENTS BELOW</span>
+                <span>*WEEKEND APPOINTMENTS*</span>
+                <span>If you are interested in booking a weekend appointment please send an email to <b>annawonw@gmail.com</b> and arrange for your weekend appointment by email. Otherwise if you are interested in a weekday appointment please use our book appointment link above.</span>
+                <div>
+                  <h3>Kutaputash</h3>
+                  <h3>(Thank You)</h3>
+                </div>
+                <div>
+                  <iframe frameborder="0" height="600" title="this a title" scrolling="no" src="https://calendar.google.com/calendar/embed?title=First%20Light%20Fun%20&amp;height=600&amp;wkst=1&amp;bgcolor=%23cc33cc&amp;src=ne64e8t3a1r26hqfipd5s4iuvg%40group.calendar.google.com&amp;color=%23853104&amp;ctz=America%2FNew_York" width="600"></iframe>
+                  <button aria-controls="sharing-popup-Blog1-footer-1-5380163201403967465" aria-label="Share" class="sharing-button touch-icon-button" id="sharing-button-Blog1-footer-1-5380163201403967465" aria-expanded="false" aria-haspopup="true">
+                    Share
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div >
+      </div >
+    </main >
+  )
+}
+
+export default BookEvent;
