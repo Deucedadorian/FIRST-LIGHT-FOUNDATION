ed0e37be1ae5dce61ecce7830937639312572faa
Dylan Cole <logboarddjc@gmail.com>
2021-10-18 22:41:11 -0400

    remove display flex from .App class to center all pages

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="App centered-bottom">
       <Aside handlePageChange={handlePageChange}
         setHeader={setHeader} 
         foundationHeader={foundationHeader} 
